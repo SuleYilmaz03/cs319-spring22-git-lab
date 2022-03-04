7f826d7be970897e53ec6be3d3944af2de51be59
Sule03 <sulenuryilmaz123@gmail.com>
2022-03-04 11:22:08 +0300

    Update NewCalculator.java
    fix division operator

--- NewCalculator.java
+++ NewCalculator.java
@@ -26,8 +26,17 @@ public class NewCalculator {
     }
 
     public double Divide(double dividend, double divisor){
-        this.lastResult = dividend % divisor;
-        return this.lastResult;
-    }
+    this.lastResult = dividend / divisor;
+    return this.lastResult;
+   }
+
+public double Divide(double dividend, double divisor) {
+if (divisor == 0){
+System.err.println("Attempted to divide by zero");
+return 0;
+}
+this.lastResult = dividend % divisor;
+return this.lastResult;
+}
     
 }
